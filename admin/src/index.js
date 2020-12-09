680351ebc974acd826ee3c8d7690efe7d74eb6f0
reznikovkg <reznikov.kosta@yandex.ru>
2020-12-09 15:48:16 +0300

    strapi v3

--- admin/src/index.js
+++ admin/src/index.js
@@ -26,19 +26,17 @@ export default strapi => {
     name: pluginPkg.strapi.name,
     preventComponentRendering: false,
     settings: {
-      global: {
-        links: [
-          {
-            title: {
-              id: getTrad('plugin.name'),
-              defaultMessage: 'Responsive image',
-            },
-            name: 'responsive-image',
-            to: `${strapi.settingsBaseURL}/responsive-image`,
-            Component: SettingsPage,
+      global: [
+        {
+          title: {
+            id: getTrad('plugin.name'),
+            defaultMessage: 'Responsive image',
           },
-        ],
-      },
+          name: 'responsive-image',
+          to: `${strapi.settingsBaseURL}/responsive-image`,
+          Component: SettingsPage,
+        },
+      ],
     },
     trads,
   };
